e340c84342290513acdfe74311903e6886b380dd
Roy Thomas <roythomascormac@gmail.com>
2024-02-18 12:20:52 +0530

    Finished

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,22 +23,13 @@ function Nav() {
                     <a href='/'>Home</a>
                 </li>
                 <li>
-                    <a href='/'>About</a>
-                </li>
-                <li>
-                    <a href='/'>Services</a>
-                </li>
-                <li>
-                    <a href='/'>Menu</a>
-                </li>
-                <li>
                     <a href='/'>Reservations</a>
                 </li>
                 <li>
                     <a href='/'>Order Online</a>
                 </li>
                 <li>
-                    <a href='/'>Login</a>
+                    <a href='/'>Contact</a>
                 </li>
             </ul>
         </nav>
